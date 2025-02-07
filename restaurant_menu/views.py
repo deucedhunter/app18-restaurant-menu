8e511734c36e71dac72d0f931632ece558b12f8e
deucedhunter <deucedhunter@gmail.com>
2025-02-07 14:00:47 +0100

    Day 56: Add class based views, add templates, add routing

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item
 
-# Create your views here.
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_created")
+    template_name = "index.html"
+
+    def get_context(self):
+        pass
+
+
+class MenuItemDetail(generic.ListView):
+    model = Item
+    template_name = "menu_item_detail.html"
